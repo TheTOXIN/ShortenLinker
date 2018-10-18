4b3c4973fb11823461ed3e8d8ccd6d39d3edbc6e
toxin <yuri.belousov.1998@gmail.com>
2018-10-18 18:38:05 +0300

    fix dto

--- src/main/java/com/toxin/shorten/dto/ShorterRequest.java
+++ src/main/java/com/toxin/shorten/dto/ShorterRequest.java
@@ -1,5 +1,15 @@
 package com.toxin.shorten.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ShorterRequest {
+
+    private String link;
+    private String baseUrl;
 
 }
--- src/main/java/com/toxin/shorten/dto/ShorterResponse.java
+++ src/main/java/com/toxin/shorten/dto/ShorterResponse.java
@@ -1,5 +1,15 @@
 package com.toxin.shorten.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ShorterResponse {
-    
+
+    private String pathQR;
+    private String hashLink;
+
 }
