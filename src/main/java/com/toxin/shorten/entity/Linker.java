6b13f8353404a670a46e4e22042510795da072d9
TheTOXIN <thetoksin@gmail.com>
2018-10-20 14:39:04 +0300

    fix link length

--- src/main/java/com/toxin/shorten/entity/Linker.java
+++ src/main/java/com/toxin/shorten/entity/Linker.java
@@ -18,7 +18,7 @@ public class Linker {
     @GeneratedValue
     private UUID id;
 
-    @Column(length = 500, nullable = false)
+    @Column(length = 1000, nullable = false)
     private String link;
 
     @Lob
